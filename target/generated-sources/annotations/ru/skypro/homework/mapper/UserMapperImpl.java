388fd3b2364d3126f1b3dce933513ae2dafa00c1
m.zhitenev <m.zhitenev23@gmail.com>
2024-01-12 18:15:59 +0300

    Переименовал мапперы

--- target/generated-sources/annotations/ru/skypro/homework/mapper/UserMapperImpl.java
+++ target/generated-sources/annotations/ru/skypro/homework/mapper/UserMapperImpl.java
@@ -9,7 +9,7 @@ import ru.skypro.homework.model.User;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-01-12T16:42:47+0300",
+    date = "2024-01-12T18:15:28+0300",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 19.0.2 (Oracle Corporation)"
 )
 @Component
