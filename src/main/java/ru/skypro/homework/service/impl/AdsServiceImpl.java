6420682f57c4bb95377d5a4981fd145fa93c12d6
m.zhitenev <m.zhitenev23@gmail.com>
2024-01-18 16:40:40 +0300

    Покрыл JavaDoc AdsServiceImpl

--- src/main/java/ru/skypro/homework/service/impl/AdsServiceImpl.java
+++ src/main/java/ru/skypro/homework/service/impl/AdsServiceImpl.java
@@ -26,6 +26,10 @@ import java.io.IOException;
 import java.util.List;
 import javax.transaction.Transactional;
 
+/**
+ * <b>Сервис объявлений</b>
+ * Хранит основную логику по работе с объявлениями
+ */
 @Service
 @RequiredArgsConstructor
 public class AdsServiceImpl implements AdsService {
@@ -36,12 +40,29 @@ public class AdsServiceImpl implements AdsService {
     private final AdMapper adMapper;
     private final ImageService imageService;
 
+    /**
+     * <b>Метод получения всех объявлений </b> <p>
+     * (В виде объекта DTO {@link AdsDto} )
+     */
     @Override
     public AdsDto getAllAds() {
         List<Ad> ads = adRepository.findAll();
         return adMapper.adListToAds(ads);
     }
 
+    /**
+     * <b>Метод добавления объявления в БД </b> <p>
+     * Принцип работы:<p>
+     * На основе минимальной информации DTO {@link CreateOrUpdateAd},
+     * создать полноценный объект объявления {@link Ad},
+     * на основе данных аутентификации создать объект пользователя {@link User},
+     * добавить созданного пользователя и картинку из параметра в объект объявления,
+     * сохранить получившийся объект с помощью репозитория {@link AdRepository},
+     *
+     * @param  createOrUpdateAd DTO объявления {@link CreateOrUpdateAd} <p>
+     * @param  image картинка товара {@link MultipartFile} <p>
+     * @param  authentication переменная аутентификации хранящая данные об авторизованном пользователе {@link Authentication} <p>
+     */
     @Override
     public AdDto addAd(CreateOrUpdateAd createOrUpdateAd, MultipartFile image, Authentication authentication) throws IOException {
         Ad ad = adMapper.createOrUpdateAdToAd(createOrUpdateAd);
@@ -54,11 +75,32 @@ public class AdsServiceImpl implements AdsService {
         return adMapper.adToAdDto(ad);
     }
 
+    /**
+     * <b>Метод получения объявления по ID </b> <p>
+     * Запрошенный {@link Ad} преобразуется в DTO {@link ExtendedAd} и возвращается методом.
+     */
     @Override
     public ExtendedAd getAd(long id) {
         return adMapper.toExtendedAd(adRepository.findById(id).orElseThrow(()->
                 new NotFoundException("Объявление с ID = " + id + " не найдено ")));
     }
+
+    /**
+     * <b> Метод удаления объявления </b> <p>
+     * Метод использует аннотацию {@link Transactional} <p>
+     * Принцип работы:<p>
+     * 1) По переданному ID находится объявление <p>
+     * 2) Проводится проверка на доступ к редактированию объявления
+     * через метод {@link #checkPermit(Ad, Authentication)} <p>
+     * 3) В репозиториях {@link CommentRepository} , {@link ImageRepository} , {@link AdRepository} - вызываются методы удаления
+     * комментариев объявления, картинки объявления, и данных об объявлении соответственно.
+     *
+     * @param id id объявления (long)
+     * @param authentication объект аутентификации с данными текущего пользователя
+     */
+    //Метод использует аннотацию @Transactional, которая дает понять Spring что данный метод - это не поочередные
+    //самостоятельные действия внутри, а единая транзакция с возможностью отката. Spring сам реализует запросы к БД
+    //и "упаковку" в транзакции. Тем самым мы обезопасим наше удаление от непредвиденных сбоев и неточностей.
 
     @Override
     @Transactional
@@ -73,6 +115,18 @@ public class AdsServiceImpl implements AdsService {
         adRepository.deleteById(id);
     }
 
+    /**
+     * <b> Метод изменения данных объявления </b> <p>
+     * (Названия, описания и цены)  {@link CreateOrUpdateAd} <p>
+     * Принцип работы:<p>
+     * Находит по ID объявление, уточняет есть ли у пользователя доступ к редактированию {@link #checkPermit(Ad, Authentication)} ,
+     *  далее меняет данные (Названия, описания и цены), сохраняет объявление, возвращает DTO только что измененного
+     *  объекта {@link AdDto}
+     * @param id id объявления (long)
+     * @param createOrUpdateAd DTO объявления {@link CreateOrUpdateAd}
+     * @param authentication объект аутентификации с данными текущего пользователя
+     * @return {@link AdDto} DTO объявления
+     */
     @Override
     public AdDto updateAd(long id, CreateOrUpdateAd createOrUpdateAd, Authentication authentication) {
         Ad ad = adRepository.findById(id).orElseThrow(() ->
@@ -85,6 +139,14 @@ public class AdsServiceImpl implements AdsService {
         return adMapper.adToAdDto(ad);
     }
 
+    /**
+     * <b> Метод возвращающий все объявления пользователя </b> <p>
+     * Принцип работы:<p>
+     * Из {@link Authentication} достает пользователя, по данному пользователю находим все его объявления.
+     * Маппер пакует лист объявлений в  {@link AdsDto} и возвращает DTO
+     * @param authentication объект аутентификации с данными текущего пользователя
+     * @return {@link AdsDto} DTO всех объявлений пользователя
+     */
     @Override
     public AdsDto getAdsMe(Authentication authentication) {
         User user = new GetAuthentication().getAuthenticationUser(authentication.getName());
@@ -92,6 +154,21 @@ public class AdsServiceImpl implements AdsService {
         return adMapper.adListToAds(adList);
     }
 
+    /**
+     * <b> Метод изменения изображения объявления </b> <p>
+     * Метод использует аннотацию {@link Transactional} <p>
+     * Принцип работы:<p>
+     * Найти объявление по ID, далее проверить доступ к редактированию, из объявления взять текущую картинку,
+     * сохранить её отдельно, на её место сохранить новую, не актуальную картинку - удалить, сохранить объект объявления,
+     * в котором уже будет новая картинка.
+     * @param id id объявления (long)
+     * @param image файл  {@link MultipartFile} (картинка объявления)
+     * @param authentication объект аутентификации с данными текущего пользователя
+     * @throws IOException (может выкинуть ошибки загрузки)
+     */
+    //Метод использует аннотацию @Transactional, которая дает понять Spring что данный метод - это не поочередные
+    //самостоятельные действия внутри, а единая транзакция с возможностью отката. Spring сам реализует запросы к БД
+    //и "упаковку" в транзакции. Тем самым мы обезопасим наше удаление от непредвиденных сбоев и неточностей.
     @Override
     @Transactional
     public void updateAdImage(Long id, MultipartFile image, Authentication authentication) throws IOException {
@@ -103,7 +180,15 @@ public class AdsServiceImpl implements AdsService {
         imageService.removeImage(imageFile);
         adRepository.save(ad);
     }
-
+    /**
+     * <b> Метод проверки доступа к редактированию объявления </b> <p>
+     * Служебный внутренний метод принимающий на вход: <p> {@link Ad} и {@link Authentication} <p>
+     * далее сравнивает автора объявления и текущего пользователя, а также проверяет, является ли пользователь Админом.
+     * Если текущий пользователь не автор объявления и не админ, то будет выброшено  {@link AccessDeniedException}
+     *
+     * @param ad объявление
+     * @param authentication объект аутентификации с данными текущего пользователя
+     */
     public void checkPermit(Ad ad, Authentication authentication){
         if (!ad.getAuthor().getEmail().equals(authentication.getName())
                 && !authentication.getAuthorities().contains(new SimpleGrantedAuthority("ADMIN"))) {
